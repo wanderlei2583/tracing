5170fa322eff5bec238a11f0048b98b27cc85648
Wanderlei Pereira <devwanderlei@gmail.com>
2024-10-31 18:31:11 -0300

    função newdephandler

--- service-a/internal/handlers/cep_handler.go
+++ service-a/internal/handlers/cep_handler.go
@@ -1,1 +1,22 @@
 package handlers
+
+import (
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/trace"
+)
+
+type CEPHandler struct {
+	serviceBURL string
+	tracer      trace.Tracer
+}
+
+type CEPRequest struct {
+	CEP string `json:"cep"`
+}
+
+func NewCEPHandler(serviceBURL string) *CEPHandler {
+	return &CEPHandler{
+		serviceBURL: serviceBURL,
+		tracer:      otel.Tracer("service-a"),
+	}
+}
