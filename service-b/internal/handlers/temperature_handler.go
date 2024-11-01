7c32777cc14f220762bd7ac44c72c30bb9f6ab94
Wanderlei Pereira <devwanderlei@gmail.com>
2024-10-31 22:50:28 -0300

    tipos adicionados

--- service-b/internal/handlers/temperature_handler.go
+++ service-b/internal/handlers/temperature_handler.go
@@ -1,1 +1,20 @@
 package handlers
+
+import (
+	"go.opentelemetry.io/otel/trace"
+
+	"service-b/internal/services"
+)
+
+type TemperatureHandler struct {
+	cepService     *services.CEPService
+	weatherService *services.WeatherService
+	tracer         trace.Tracer
+}
+
+type TemperatureResponse struct {
+	City  string  `json:"city"`
+	TempC float64 `json:"temp_C"`
+	TempF float64 `json:"temp_F"`
+	TempK float64 `json:"temp_K"`
+}
