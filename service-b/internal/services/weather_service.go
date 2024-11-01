e4a15540a24d9d41d99feeb64b4e8ddd6947ce78
Wanderlei Pereira <devwanderlei@gmail.com>
2024-10-31 23:01:07 -0300

    função gettemperature

--- service-b/internal/services/weather_service.go
+++ service-b/internal/services/weather_service.go
@@ -1,5 +1,14 @@
 package services
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"go.opentelemetry.io/otel"
+)
+
 type WeatherService struct {
 	apiKey string
 }
@@ -12,4 +21,30 @@ type weatherResponse struct {
 
 func NewWeatherService(apiKey string) *WeatherService {
 	return &WeatherService{apiKey: apiKey}
+}
+
+func (s *WeatherService) GetTemperature(
+	ctx context.Context,
+	city string,
+) (float64, error) {
+	ctx, span := otel.Tracer("service-b").Start(ctx, "get_temperature")
+	defer span.End()
+
+	url := fmt.Sprintf(
+		"http://api.weatherapi.com/v1/current.json?key=%s&q=%s",
+		s.apiKey,
+		city,
+	)
+	resp, err := http.Get(url)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	var weather weatherResponse
+	if err := json.NewDecoder(resp.Body).Decode(&weather); err != nil {
+		return 0, err
+	}
+
+	return weather.Current.TempC, nil
 }
