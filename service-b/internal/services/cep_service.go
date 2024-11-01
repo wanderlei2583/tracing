381acb9b163cb2bf6fac7aeace4a7e6b81598f8e
Wanderlei Pereira <devwanderlei@gmail.com>
2024-10-31 22:57:54 -0300

    função getcity

--- service-b/internal/services/cep_service.go
+++ service-b/internal/services/cep_service.go
@@ -1,6 +1,14 @@
 package services
 
-import "errors"
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+
+	"go.opentelemetry.io/otel"
+)
 
 var (
 	ErrInvalidCEP  = errors.New("invalid CEP")
@@ -18,4 +26,30 @@ type viaCEPResponse struct {
 
 func NewCEPService(baseURL string) *CEPService {
 	return &CEPService{baseURL: baseURL}
+}
+
+func (s *CEPService) GetCity(ctx context.Context, cep string) (string, error) {
+	ctx, span := otel.Tracer("service-b").Start(ctx, "get_city_from_cep")
+	defer span.End()
+
+	resp, err := http.Get(fmt.Sprintf("%s%s/json", s.baseURL, cep))
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusBadRequest {
+		return "", ErrInvalidCEP
+	}
+
+	var cepResp viaCEPResponse
+	if err := json.NewDecoder(resp.Body).Decode(&cepResp); err != nil {
+		return "", err
+	}
+
+	if cepResp.Erro {
+		return "", ErrCEPNotFound
+	}
+
+	return cepResp.City, nil
 }
