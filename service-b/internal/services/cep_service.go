240964cbbe521681b43d79518ccddc2f3f155669
Wanderlei Pereira <devwanderlei@gmail.com>
2024-10-31 22:55:26 -0300

    inserindo as variaveis e os tipos

--- service-b/internal/services/cep_service.go
+++ service-b/internal/services/cep_service.go
@@ -1,1 +1,17 @@
 package services
+
+import "errors"
+
+var (
+	ErrInvalidCEP  = errors.New("invalid CEP")
+	ErrCEPNotFound = errors.New("CEP not found")
+)
+
+type CEPService struct {
+	baseURL string
+}
+
+type viaCEPResponse struct {
+	City string `json:"localidade"`
+	Erro bool   `json:"erro"`
+}
